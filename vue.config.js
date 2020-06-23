cc107623d7960466010f061f87ca5757a05767c9
takapdayon <business.takap@gmail.com>
2020-06-23 15:33:09 +0000

    pala

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "devServer": {
+    "proxy": {
+      "/api/": {
+        "target": "https://public-api.tracker.gg/",
+        "pathRewrite": {
+          "^/api/": ""
+        }
+      }
+    }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
+}
